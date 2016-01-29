c7d4e0a9bad9396315e5514a0f968621965b9064
Polina <polina.mihova@yahoo.com>
2016-01-28 23:24:04 -0800

    Create passing specs for pizza order

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function Kind (type1, type2) {
+  this.type1 = type1;
+  this.type2 = type2;
+}
+
+function Topping (meattop, veggietop,maintop) {
+  this.meattop = meattop;
+  this.veggietop = veggietop;
+  this.maintop = maintop;
+
+}
+
+function Size (sizechoice1, sizechoice2, sizechoice3) {
+  this.sizechoice1 = sizechoice1;
+  this.sizechoice2 = sizechoice2;
+  this.sizechoice3 = sizechoice3;
+}
